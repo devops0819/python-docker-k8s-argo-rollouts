2e570c2356ac71fef575c822bb98fdfa52f9bb24
devops0819 <163329072+devops0819@users.noreply.github.com>
2024-03-17 13:38:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,12 @@ def color_grid():
               for i in range(100)]
     return render_template('grid.html', colors=colors)
 
+@app.route('/hello')
+def color_grid_api():
+    colors = ['#'+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+              for i in range(100)]
+    value = colors['srr']
+    return render_template('grid.html', colors=colors)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
