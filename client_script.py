eb4a3862890084feae522338ced2b86185f6a481
devops0819 <devops0819@gmail.com>
2024-03-17 15:59:43 +0530

    udpated rollout

--- client_script.py
+++ client_script.py
@@ -1,5 +1,5 @@
 import requests
 import time
 
-for i in range(1, 1000):
+for i in range(1, 10000):
     requests.get('http://a9da78c8e751a4b4b926f843bd9d1a96-1816739119.ap-south-1.elb.amazonaws.com:5000/')
