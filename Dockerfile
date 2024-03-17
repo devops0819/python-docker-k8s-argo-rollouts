58fade218ec9d76561a73b3a791674053990d134
thekoti <devops0819@gmail.com>
2024-03-17 13:54:51 +0530

    updated dockerfile eith devops

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 
 COPY . /app
 
-RUN pip install Flask
+RUN pip install -r requirements.txt
 
 EXPOSE 5000
 
--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
-import random
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
+# Initialize Prometheus metrics exporter for Flask app
+metrics = PrometheusMetrics(app)
+
+# Static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
+
 @app.route('/')
-def color_grid():
-    colors = ['#'+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-              for i in range(100)]
-    return render_template('grid.html', colors=colors)
+def main():
+    return "Hello, world!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
