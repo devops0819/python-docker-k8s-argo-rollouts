2d2950a3a426bdccaa765c74e3ee4fdeebdd91bf
devops0819 <devops0819@gmail.com>
2024-03-19 22:21:22 +0530

    client.py changes

--- client_script.py
+++ client_script.py
@@ -1,5 +1,17 @@
 import requests
 import time
 
-for i in range(1, 10000):
-    requests.get('http://a9da78c8e751a4b4b926f843bd9d1a96-1816739119.ap-south-1.elb.amazonaws.com:5000/')
+url = 'http://a9da78c8e751a4b4b926f843bd9d1a96-1816739119.ap-south-1.elb.amazonaws.com:5000/'
+
+for i in range(1, 10):
+    # requests.get('http://a9da78c8e751a4b4b926f843bd9d1a96-1816739119.ap-south-1.elb.amazonaws.com:5000/')
+    # print (requests.Response.status_code)
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Print the content of the response
+        print(response.text)
+    else:
+        # Print the status code if the request was not successful
+        print(f"Failed to retrieve content: HTTP {response.status_code}")
