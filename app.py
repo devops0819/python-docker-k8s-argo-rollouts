bf42793832e534d928a945ce3959ee978779bca0
devops0819 <163329072+devops0819@users.noreply.github.com>
2024-03-25 20:54:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,15 @@ from prometheus_flask_exporter import PrometheusMetrics
 import random
 
 app = Flask(__name__)
-
-# Initialize Prometheus metrics exporter for Flask app
 metrics = PrometheusMetrics(app)
-
-# Static information as metric
 metrics.info('app_info', 'Application info', version='1.0.3')
 
 @app.route('/')
 def main():
-    # Decide whether to return an error or the normal response
     if random.random() < 0.2:  # 20% chance
-        abort(500)  # HTTP 500 Internal Server Error
+        abort(500)
     return "Hello, world!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
